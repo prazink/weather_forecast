a9edf69f050150af1aba69eb51e08913b7065344
Prazin Karki <prazink@gmail.com>
2019-12-22 19:38:19 +1100

    Updates

---
+++ precache-manifest.072d9bb5b37943c36b49bc713a783c39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1c6b6095ec47a5404bb12264c9efafa",
+    "url": "/weather_forecast/index.html"
+  },
+  {
+    "revision": "b5496b4dbdcefab9ea0d",
+    "url": "/weather_forecast/static/css/main.2187afd2.chunk.css"
+  },
+  {
+    "revision": "bfb9151ed37b90b5cab4",
+    "url": "/weather_forecast/static/js/2.08db1212.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/weather_forecast/static/js/2.08db1212.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b5496b4dbdcefab9ea0d",
+    "url": "/weather_forecast/static/js/main.3a4b1865.chunk.js"
+  },
+  {
+    "revision": "b1b7f28beef7a4a72fa7",
+    "url": "/weather_forecast/static/js/runtime-main.453adcbc.js"
+  }
+]);
